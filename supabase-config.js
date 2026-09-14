5a978bff13faffa85b87bd04a5056520a689b886
Nithinkumar-afk <219115046+Nithinkumar-afk@users.noreply.github.com>
2026-09-14 14:12:46 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,11 +1,41 @@
+// ============================================
+// PRINTFLOW SUPABASE CONFIG
+// ============================================
+
 const SUPABASE_URL =
-    "https://cfppcswvfvkevbtachrw.supabase.co/rest/v1/";
+    "https://cfppcswvfvkevbtachrw.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY =
     "sb_publishable_iRZybbXYZSA7MORMBu4dZA_mhyASvNY";
 
-const supabaseClient =
-    window.supabase.createClient(
-        SUPABASE_URL,
-        SUPABASE_PUBLISHABLE_KEY
+
+// ============================================
+// CHECK SUPABASE LIBRARY
+// ============================================
+
+if (
+    typeof window.supabase === "undefined"
+) {
+
+    console.error(
+        "PrintFlow: Supabase library was not loaded."
     );
+
+} else {
+
+    // ========================================
+    // CREATE SUPABASE CLIENT
+    // ========================================
+
+    window.supabaseClient =
+        window.supabase.createClient(
+            SUPABASE_URL,
+            SUPABASE_PUBLISHABLE_KEY
+        );
+
+
+    console.log(
+        "PrintFlow: Supabase client initialized."
+    );
+
+}
